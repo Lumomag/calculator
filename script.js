ee6551f349e7b825635a1c2d18ecacc3d6cfa74a
Lumomag <lucasmoraesmagalhaes@gmail.com>
2024-04-03 12:09:50 -0300

    Inital setup

---
+++ script.js
@@ -0,0 +1,42 @@
+function add(first, second){
+  return first + second;
+}
+
+function subtract(first, second){
+  return first - second;
+}
+
+function multiply(first, second){
+  return first * second;
+}
+
+function divide(first, second){
+  return first / second;
+}
+
+function operate(firstNumb, operator, secondNumb){
+  switch (operator) {
+    case '+':
+      add(firstNumb, secondNumb);
+      break;
+    
+    case '-':
+      subtract(firstNumb, secondNumb);
+      break;
+
+    case '*':
+      multiply(firstNumb, secondNumb);
+      break;
+
+    case '/':
+      divide(firstNumb, secondNumb);
+      break;
+
+    default: alert('Invalid operation');
+      break;
+  }
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
